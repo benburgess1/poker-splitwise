113dd900af631869202c6f8610066b90390f5cf5
benburgess1 <ben.burgess@hotmail.co.uk>
2025-07-16 09:47:20 +0100

    Remove init_db.py and test.py

--- init_db.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-# from app import Game, Player, BuyIn, Winner  # replace with your actual models
-
-with app.app_context():
-    db.create_all()
-    print("Database tables created successfully.")
